1f48d01f13f7a33d42a40f3a4062f190850597cc
fearlessfreap24 <dylanaperez@yahoo.com>
2021-01-08 14:37:25 -0600

    tested with alphabet and made some minor changes

--- Heaps.py
+++ Heaps.py
@@ -13,16 +13,16 @@ class Heaps:
         
         def add(self, data):
             self.dataArr.append(data)
-            self.trickleUp(self.lastPos)
+            self.trickleUp(len(self.dataArr) - 1)
             self.lastPos += 1
 
 
         def remove(self):
             data = self.dataArr[0]
-            self.dataArr[0] = self.dataArr[self.lastPos - 1]
-            self.lastPos -= 1
-            self.trickleDown(0)
+            self.dataArr[0] = self.dataArr[-1]
             self.dataArr.pop()
+            self.lastPos = len(self.dataArr) - 1
+            self.trickleDown(0)
             return data
 
 
@@ -74,16 +74,16 @@ class Heaps:
         
         def add(self, data):
             self.dataArr.append(data)
-            self.trickleUp(self.lastPos)
-            self.lastPos += 1
+            self.trickleUp(len(self.dataArr) - 1)
+            self.lastPos = len(self.dataArr)
 
 
         def remove(self):
             data = self.dataArr[0]
-            self.dataArr[0] = self.dataArr[self.lastPos - 1]
-            self.lastPos -= 1
-            self.trickleDown(0)
+            self.dataArr[0] = self.dataArr[-1]
             self.dataArr.pop()
+            self.lastPos = len(self.dataArr) - 1
+            self.trickleDown(0)
             return data
 
 
@@ -107,38 +107,42 @@ class Heaps:
             left = 2*parent+1
             right = 2*parent+2
             
-            if left > self.lastPos - 1 and right < self.lastPos -1:
+            if left > self.lastPos - 1 and right > self.lastPos -1:
                 return
 
             if left == (self.lastPos - 1):
-                if self.dataArr[parent] < self.dataArr[left]:
+                if self.dataArr[parent] > self.dataArr[left]:
                     self.swap(parent, left)
                 return
             if right == (self.lastPos - 1):
-                if self.dataArr[parent] < self.dataArr[right]:
+                if self.dataArr[parent] > self.dataArr[right]:
                     self.swap(parent, right)
                 return
-            if self.dataArr[left] < self.dataArr[right] and self.dataArr[parent] > self.dataArr[left]:
-                self.swap(parent, left)
-                self.trickleDown(left)
-            elif self.dataArr[parent] < self.dataArr[right]:
-                self.swap(parent, right)
-                self.trickleDown(right)
+            if self.dataArr[left] < self.dataArr[right]:
+                if self.dataArr[parent] > self.dataArr[left]:
+                    self.swap(parent, left)
+                    self.trickleDown(left)
+            else:
+                if self.dataArr[parent] > self.dataArr[right]:
+                    self.swap(parent, right)
+                    self.trickleDown(right)
 
 
 if __name__ == "__main__":
     
     import random
+    testrange = 100
+    # heap = Heaps.MinHeap()
+    heap = Heaps.MaxHeap()
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for i in range(testrange):
+        heap.add(alphabet[random.randrange(26)])
 
-    minheap = Heaps.MinHeap()
-    for i in range(100):
-        minheap.add(random.randrange(100))
-
-    print(minheap.dataArr)
+    print(heap.dataArr)
 
     
-    for i in range(100):
-        print(minheap.remove())
-    print(minheap.dataArr)
+    for i in range(testrange):
+        print(heap.remove())
+    print(heap.dataArr)
     
     
